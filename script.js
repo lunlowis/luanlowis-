77fef71506d4897d7237055fd46688c92d856ffb
lunlowis <167491648+lunlowis@users.noreply.github.com>
2024-05-01 12:19:56 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var btn = document.querySelector('.yes-btn');
+    btn.addEventListener('click', function() {
+        window.location.href = 'pink-page.html'; // Chuyển hướng đến trang có màu hồng
+    });
+});
